50627dcee3258c1699a31baecd83b616ef758123
Frans Holwerda <fholwerd@f1r4s18.codam.nl>
2024-01-06 15:19:10 +0100

    this is a test for game

---
+++ gametest/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function(){
+	const canvas = document.getElementById('canvas1');
+	const ctx = canvas.getContext('2d');
+	canvas.width = 500;
+	canvas.height = 500;
+
+	class Game {
+		constructor(width, height){
+			this.width = width;
+			this.height = height;
+		}
+		update(){
+
+		}
+		draw(){
+			
+		}
+	}
+});
---
+++ gametest/player.js
@@ -0,0 +1,5 @@
+class Player {
+	constructor(game){
+		this.game = game;
+	}
+}
