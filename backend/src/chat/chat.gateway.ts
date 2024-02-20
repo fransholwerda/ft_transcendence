aadd42137523238802f391483ab0420f9dce204d
Frans Holwerda <fransholwerda@Franss-MacBook-Pro.local>
2024-02-20 17:03:40 +0100

    converted to typescript (chat not finished)

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,12 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+
+@WebSocketGateway(8001, { cors: '*' })
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log(message);
+    this.server.emit('message', message);
+  }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import io, { Socket } from "socket.io-client"
+import MessageInput from './chat/MessageInput'
+import Messages from './chat/Messages'
+
+function App() {
+  const [socket, setSocket] = useState<Socket>()
+  const[messages, setMessages] = useState<string[]>([])
+
+  const send = (value: string) => {
+    socket?.emit("message", value)
+  }
+  useEffect(() => {
+    const newSocket=io("http://localhost:8001")
+    setSocket(newSocket)
+  },[setSocket])
+
+  const messageListener = (message: string) => {
+    setMessages([...messages, message])
+  }
+  useEffect(() => {
+    socket?.on("message", messageListener)
+    return () => {
+      socket?.off("message", messageListener)
+    }
+  },[messageListener])
+  return (
+    <>
+      {" "}
+      <MessageInput send={send}/>
+      <Messages messages={messages}/>
+    </>
+  )
+}
+
+export default App
