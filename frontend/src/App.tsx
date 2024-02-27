8e7b8cc646dc71ab181ad6970094e951ead0ad21
Frans Holwerda <fholwerd@f1r4s16.codam.nl>
2024-02-27 18:48:29 +0100

    Adding a simple sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import io, { Socket } from "socket.io-client"
 import MessageInput from './chat/MessageInput'
 import Messages from './chat/Messages'
 import LoginForm from './Login/LoginForm'
+import Sidebar from './sidebar/sidebar'
 
 function App() {
   const [socket, setSocket] = useState<Socket>()
-  const[messages, setMessages] = useState<string[]>([])
+//   const[messages, setMessages] = useState<string[]>([])
+  const [messages, setMessages] = useState<{ sender: string; text: string }[]>([]);
 
   const send = (value: string) => {
     socket?.emit("message", value)
@@ -18,7 +20,8 @@ function App() {
   },[setSocket])
 
   const messageListener = (message: string) => {
-    setMessages([...messages, message])
+    // setMessages([...messages, message])
+	setMessages((prevMessages) => [...prevMessages, { sender: 'You', text: message }]);
   }
   useEffect(() => {
     socket?.on("message", messageListener)
@@ -27,12 +30,19 @@ function App() {
     }
   },[messageListener])
   return (
-    <>
-      {" "}
-      <MessageInput send={send}/>
-      <Messages messages={messages}/>
-      {/* <LoginForm/> */}
-    </>
+	<div>
+      <Sidebar messages={messages} />
+      <div className="chat-container">
+	    <MessageInput send={send}/>
+      </div>
+    </div>
+    // <>
+    //   {" "}
+    //   <LoginForm/>
+    //   <MessageInput send={send}/>
+    //   {/* <Messages messages={messages}/> */}
+	//   <Sidebar messages={messages}/>
+    // </>
   )
 }
 
