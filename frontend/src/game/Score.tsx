0916776577c2cbf2828092e7e1a7df72c4552a8e
Kevin Kroon <kkroon@student.codam.nl>
2024-06-18 16:31:12 +0200

    in seperate files now

---
+++ frontend/src/game/Score.tsx
@@ -0,0 +1,18 @@
+// Score.tsx
+export class Score {
+	left: number;
+	right: number;
+  
+	constructor() {
+	  this.left = 0;
+	  this.right = 0;
+	}
+  
+	draw(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+	  context.fillStyle = 'white';
+	  context.font = '48px sans-serif';
+	  context.fillText(this.left.toString(), canvas.width / 4, 50);
+	  context.fillText(this.right.toString(), (canvas.width / 4) * 3, 50);
+	}
+  }
+  
