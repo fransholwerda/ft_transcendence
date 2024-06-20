a594af8b5a970165e33e71f78eaf531e6c353169
Kevin Kroon <kevinjankroon@gmail.com>
2024-06-20 23:02:33 +0200

    using a kind of wrapper now

--- frontend/src/grid/grid.tsx
+++ frontend/src/grid/grid.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './grid.css';
 import App from '../App';
-import Game from '../game/game';
+import GameManager from '../gameManager/GameManager';
 
 const Grid: React.FC = () => {
     return (
         <div className="parent">
             <div className="header"></div>
-            <div className="game"><Game /></div>
+            <div className="game"><GameManager /></div>
             <div className="chat"><App /></div>
         </div>
     );
