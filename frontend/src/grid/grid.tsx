c79224c3251b272a50a48e29137ceb292d27d121
you@example.com <you@example.com>
2024-07-04 16:45:01 +0200

    Added login. No scroll

--- frontend/src/grid/grid.tsx
+++ frontend/src/grid/grid.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './grid.css';
 import App from '../App';
 import GameManager from '../gameManager/GameManager';
+import Header from '../Login/LoginForm';
 
 const Grid: React.FC = () => {
     return (
         <div className="parent">
-            <div className="header"></div>
+            <div className="header"><Header /></div>
             <div className="game"><GameManager /></div>
             <div className="chat"><App /></div>
         </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Grid from './grid/grid'
 import './grid/grid.css'
-import Loginf from './Login/LoginForm'
-import './Login/LoginForm.css'
+// import Loginf from './Login/LoginForm'
+// import './Login/LoginForm.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 
-    <Loginf>
-    </Loginf>
+    {/* <Loginf>
+    </Loginf> */}
     <Grid>
     </Grid>
   </React.StrictMode>,
