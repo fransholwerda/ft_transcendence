e1a6298fc10f2aaab01f4a58d5c24c2b885eae31
Kevin Kroon <kevinjankroon@gmail.com>
2024-07-13 13:02:34 +0200

    rm unused commented out things

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import MainGrid from './mainGrid/MainGrid'
 import './mainGrid/MainGrid.css'
-// import Loginf from './Login/LoginForm'
-// import './Login/LoginForm.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-    {/* <Loginf>
-    </Loginf> */}
     <MainGrid>
     </MainGrid>
   </React.StrictMode>,
--- frontend/src/mainGrid/MainGrid.tsx
+++ frontend/src/mainGrid/MainGrid.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './MainGrid.css';
-// import App from '../App';
 import Chat from '../chat/Chat'
 import GameManager from '../gameManager/GameManager';
 
@@ -9,7 +8,6 @@ const MainGrid: React.FC = () => {
         <div className="parent">
             <div className="header"></div>
             <div className="game"><GameManager /></div>
-            {/* <div className="chat"><App /></div> */}
 			<div className="chat"><Chat /></div>
         </div>
     );
