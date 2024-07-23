967a4688d4d0434a9418d4395f3810d7d57bff14
Danila Kocob <dkocob@f1r3s15.codam.nl>
2024-07-23 17:08:31 +0200

    Login looks better and can login as guest and Profile looks better

--- frontend/src/ProfilePage/ProfilePage.tsx
+++ frontend/src/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ProfilePage.css';
 
 interface ProfilePageProps {
   user: string;
@@ -13,10 +14,20 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ user, onMainPage, onLogout })
         <h2>Profile Page</h2>
         <p>Username: {user}</p>
         <button onClick={onMainPage}>Back to Main</button>
-        <button onClick={onLogout}>Log Out</button></div>
-          <div className="profile_friends"><h2>Profile friends</h2><h3>Friend_1</h3></div>
-          <div className="profile_av"><h2>Profile Achievements</h2><h3>Win_in_a_row_1</h3></div>
-          <div className="profile_game_history"><h2>Profile Game History</h2><h3>Game_1</h3></div>
+        <button onClick={onLogout}>Log Out</button>
+      </div>
+      <div className="profile_friends">
+        <h2>Profile Friends</h2>
+        <h3>Friend_1</h3>
+      </div>
+      <div className="profile_av">
+        <h2>Profile Achievements</h2>
+        <h3>Win_in_a_row_1</h3>
+      </div>
+      <div className="profile_game_history">
+        <h2>Profile Game History</h2>
+        <h3>Game_1</h3>
+      </div>
     </div>
   );
 };
