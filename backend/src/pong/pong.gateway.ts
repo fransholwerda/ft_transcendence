777f7c405ee4e214c98c53aff8da6f4e5bad681c
Kevin Kroon <kkroon@student.codam.nl>
2024-07-28 18:00:28 +0200

    spaces to tabs

--- backend/src/pong/pong.gateway.ts
+++ backend/src/pong/pong.gateway.ts
@@ -15,56 +15,53 @@ import { Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/pong', cors: { origin: '*' } })
 export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer()
-    server: Server;
+	@WebSocketServer()
+	server: Server;
 
-    private clients: { [key: string]: { order: number, username: string } } = {};
-    private order: number = 1;
-    private queue: string[] = []; // Queue property to keep track of clients in the queue
+	private clients: { [key: string]: { order: number, username: string } } = {};
+	private order: number = 1;
+	private queue: string[] = [];
 
-    handleConnection(client: Socket) {
-        // Assume the client sends their username immediately upon connection
-        // This could be done through a specific message the client sends after connecting
-        // For this example, we'll just initialize with a placeholder
-        this.clients[client.id] = { order: this.order++, username: 'Unknown' };
-        this.server.emit('pong', this.formatPong());
-    }
+	handleConnection(client: Socket) {
+		this.clients[client.id] = { order: this.order++, username: 'Unknown' };
+		this.server.emit('pong', this.formatPong());
+	}
 
-    handleDisconnect(client: Socket) {
-        delete this.clients[client.id];
-        this.server.emit('pong', this.formatPong());
-        this.handleLeaveQueue(client); // Remove from queue on disconnect
-    }
+	handleDisconnect(client: Socket) {
+		delete this.clients[client.id];
+		this.server.emit('pong', this.formatPong());
+		this.handleLeaveQueue(client);
+	}
 
-    @SubscribeMessage('set-username') // New handler for setting a username
-    handleSetUsername(client: Socket, username: string) {
-        if (this.clients[client.id]) {
-            this.clients[client.id].username = username;
-            this.server.emit('pong', this.formatPong()); // Update all clients with the new username
-        }
-    }
+	@SubscribeMessage('set-username')
+	handleSetUsername(client: Socket, username: string) {
+		if (this.clients[client.id]) {
+			this.clients[client.id].username = username;
+			this.server.emit('pong', this.formatPong());
+		}
+	}
 
-    @SubscribeMessage('request-Pong')
-    handleRequestPong(client: Socket) {
-        client.emit('pong', this.formatPong());
-    }
+	@SubscribeMessage('request-Pong')
+	handleRequestPong(client: Socket) {
+		client.emit('pong', this.formatPong());
+	}
 
-    @SubscribeMessage('join-queue')
-    handleJoinQueue(client: Socket) {
-        if (!this.queue.includes(client.id)) {
-            this.queue.push(client.id);
-            this.server.emit('queue-update', this.queue); // Emit updated queue to all clients
-        }
-    }
+	@SubscribeMessage('join-queue')
+	handleJoinQueue(client: Socket) {
+		if (!this.queue.includes(client.id)) {
+			this.queue.push(client.id);
+			this.server.emit('queue-update', this.queue);
+		}
+	}
 
-    @SubscribeMessage('leave-queue')
-    handleLeaveQueue(client: Socket) {
-        this.queue = this.queue.filter(id => id !== client.id);
-        this.server.emit('queue-update', this.queue); // Emit updated queue to all clients
-    }
+	@SubscribeMessage('leave-queue')
+	handleLeaveQueue(client: Socket) {
+		this.queue = this.queue.filter(id => id !== client.id);
+		this.server.emit('queue-update', this.queue);
+	}
 
-    private formatPong() {
-        return Object.entries(this.clients).map(([id, { order, username }]) => ({ id, order, username }));
-    }
+	private formatPong() {
+		return Object.entries(this.clients).map(([id, { order, username }]) => ({ id, order, username }));
+	}
 }
 
