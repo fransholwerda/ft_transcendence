f3562155c6e26bc7c65a0e9aaf2ddf51d5c91ebc
Frans Holwerda <fholwerd@f1r3s12.codam.nl>
2024-08-01 15:37:17 +0200

    foundational css in place using :root and importing this into the other css files

--- frontend/src/mainGrid/MainGrid.tsx
+++ frontend/src/mainGrid/MainGrid.tsx
@@ -4,15 +4,10 @@ import MainHeader from '../mainHeader/MainHeader';
 
 // import GameManager from '../gameManager/GameManager';
 // <GameManager />
-<<<<<<< HEAD
-import Tabs from '../chat/Tabs';
-// import Connections from '../connections/Connections';
-import ProfilePage from '../ProfilePage/ProfilePage';
-=======
 // import Connections from '../connections/Connections';
 // <Connections />
 import Pong from '../pong/Pong';
->>>>>>> main
+import Tabs from '../chat/Tabs';
 
 interface MainGridProps {
   user: string;
@@ -24,13 +19,8 @@ const MainGrid: React.FC<MainGridProps> = ({ user, onProfile, onLogout }) => {
   return (
     <div className="parent">
       <div className="header"><MainHeader user={user} onProfile={onProfile} onLogout={onLogout} /></div>
-<<<<<<< HEAD
-      <div className="game"><ProfilePage user={user} onLogout={onLogout} /></div>
-      <div className="chat"><Tabs /></div>
-=======
       <div className="game"><Pong /></div>
-      <div className="chat"><Chat /></div>
->>>>>>> main
+      <div className="chat"><Tabs /></div>
     </div>
   );
 };
