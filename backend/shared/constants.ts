6721cee91a98d485fc6cb89b29f92883ac390d5a
Kevin Kroon <kkroon@f1r3s15.codam.nl>
2024-08-02 21:09:49 +0200

    manually update hostname ip for now

--- backend/shared/constants.ts
+++ backend/shared/constants.ts
@@ -1,10 +1,10 @@
 export class Constants {
 	public static FRONTEND_HOST_PROTOCOL = 'http://';
-	public static FRONTEND_HOST_HOSTNAME = '10.11.3.12';
+	public static FRONTEND_HOST_HOSTNAME = '10.11.3.15';
 	public static FRONTEND_HOST_PORT = '3003';
 
 	public static BACKEND_HOST_PROTOCOL = 'http://';
-	public static BACKEND_HOST_HOSTNAME = '10.11.3.12';
+	public static BACKEND_HOST_HOSTNAME = '10.11.3.15';
 	public static BACKEND_HOST_PORT = '3003';
 
 	public static FRONTEND_HOST_URL = `${Constants.FRONTEND_HOST_PROTOCOL}${Constants.FRONTEND_HOST_HOSTNAME}:${Constants.FRONTEND_HOST_PORT}`;
