b41c62caf9769b8911d482c013330120ce0f2fda
Kevin Kroon <kkroon@student.codam.nl>
2024-08-03 16:03:22 +0200

    settings page template

---
+++ frontend/src/SettingsPage/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SettingsPage.css';
+
+const SettingsPage: React.FC = () => {
+  return (
+    <div className="settings-page">
+      <h1>My settings</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
