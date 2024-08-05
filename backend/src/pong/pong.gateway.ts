96285b193b3303f6c3468a24d465efbc7290e59f
Kevin Kroon <kkroon@student.codam.nl>
2024-08-05 17:42:38 +0200

    debug printing

--- backend/src/pong/pong.gateway.ts
+++ backend/src/pong/pong.gateway.ts
@@ -22,11 +22,11 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	private rooms: Map<string, string[]> = new Map();
 
 	handleConnection(client: Socket) {
-		console.log(`Client connected: ${client.id}`);
+		console.log(`Pong Client connected: ${client.id}`);
 	}
 
 	handleDisconnect(client: Socket) {
-		console.log(`Client disconnected: ${client.id}`);
+		console.log(`Pong Client disconnected: ${client.id}`);
 		this.removeFromQueue(client.id);
 		this.removeFromRoom(client.id);
 	}
@@ -36,15 +36,21 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		if (!this.queue.includes(client.id)) {
 			this.queue.push(client.id);
 			this.checkQueue();
+			console.log(`Pong Client: ${client.id} joined queue`);
+		}
+		else {
+			console.log(`Pong Client: ${client.id} is already in queue`);
 		}
 	}
 
 	@SubscribeMessage('leaveQueue')
 	handleLeaveQueue(client: Socket) {
 		this.removeFromQueue(client.id);
+		console.log(`Pong Client: ${client.id} left queue`);
 	}
 
 	private checkQueue() {
+		console.log(`Queue length: ${this.queue.length}`);
 		if (this.queue.length >= 2) {
 			const p1 = this.queue.shift();
 			const p2 = this.queue.shift();
@@ -57,14 +63,18 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 			this.server.in(p1).socketsJoin(roomId);
 			this.server.in(p2).socketsJoin(roomId);
+			console.log(`Pong Game started: ${roomId}`);
+			console.log(`Pong Game players: ${p1} vs ${p2}`);
 		}
 	}
 
 	private removeFromQueue(clientId: string) {
+		console.log(`removeFromQueue: ${clientId}`);
 		this.queue = this.queue.filter(id => id !== clientId);
 	}
 
 	private removeFromRoom(clientId: string) {
+		console.log(`removeFromRoom: ${clientId}`);
 		for (const [roomId, players] of this.rooms.entries()) {
 			if (players.includes(clientId)) {
 				this.rooms.delete(roomId);
