6e1be498bea596714837dadbce41af143c20505b
Kevin Kroon <kkroon@student.codam.nl>
2024-08-26 14:16:40 +0200

    set it intially out of the loop, use pongC for canvas size and proper css for canvas so it scales

--- frontend/src/pong/PongGame.tsx
+++ frontend/src/pong/PongGame.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { GameSession } from './PongTypes';
 import { Socket } from 'socket.io-client';
 import './Pong.css';
+import { PongC } from '../../shared/constants';
 
 // interface PongGameProps {
 //     gameSession: GameSession;
@@ -41,22 +42,22 @@ const PongGame: React.FC<PongGameProps> = ({ pSock, gameSession }) => {
             console.log(`PongGame: context is null`);
             return;
         }
+        const cw = canvas.width;
+        const ch = canvas.height;
+        // game area
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, cw, ch);
+        // game title
+        context.fillStyle = 'white';
+        const fontSize = ch / 10;
+        context.font = `${fontSize}px sans-serif`;
+        context.fillText('Pong', (cw / 2) - (cw / 10), (ch / 10));
+        // ball
+        context.fillStyle = 'white';
+        context.fillRect(gameState.ball.x, gameState.ball.y, gameState.ball.width, gameState.ball.height);
+
         const gameLoop = () => {
-            canvas.width = window.innerWidth * 0.75;
-            canvas.height = window.innerHeight * 0.85;
-            const cw = canvas.width;
-            const ch = canvas.height;
-            // game area
-			context.fillStyle = 'black';
-			context.fillRect(0, 0, cw, ch);
-            // game title
-            context.fillStyle = 'white';
-            const fontSize = ch / 10;
-            context.font = `${fontSize}px sans-serif`;
-            context.fillText('Pong', (cw / 2) - (cw / 10), (ch / 10));
-            // ball
-            context.fillStyle = 'white';
-            context.fillRect(gameState.ball.x, gameState.ball.y, gameState.ball.width, gameState.ball.height);
+            
 
             requestAnimationFrame(gameLoop);
 		};
@@ -65,7 +66,7 @@ const PongGame: React.FC<PongGameProps> = ({ pSock, gameSession }) => {
 
     return (
         <div className="game-screen">
-           <canvas ref={canvasRef} style={{ width: '75vw', height: '85vh' }} />
+           <canvas ref={canvasRef} width={PongC.CANVAS_WIDTH} height={PongC.CANVAS_HEIGHT} />
             {/* <p>p1 | P2</p>
             <p>clientid: {gameSession.p1.clientid ?? 'N/A'} | {gameSession.p2.clientid ?? 'N/A'}</p>
             <p>userid: {gameSession.p1.userid ?? 'N/A'} | {gameSession.p2.userid ?? 'N/A'}</p>
