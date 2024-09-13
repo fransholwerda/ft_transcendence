546cf49f546e4340c35569d13843740cefc58c6b
Joel Burleson <joelburleson0104@gmail.com>
2024-09-13 17:42:33 +0200

    added jwt gaurds

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: 'temp_secret',
       signOptions: { expiresIn: '1d' }
     })
   ],
   controllers: [ AuthController ],
-  providers: [ AuthService ]
+  providers: [ AuthService, JwtStrategy ]
 })
 export class AuthModule {}
---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: 'temp_secret'
+        });
+    }
+
+    validate(payload: any) {
+        return payload;
+    }
+}
