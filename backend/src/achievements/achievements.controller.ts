7cf93b315e9838b7c5da0e2dd8df572a0436543b
Alex <horlingsalexander@gmail.com>
2024-10-01 15:09:35 +0200

    more controller stuff

--- backend/src/achievements/achievements.controller.ts
+++ backend/src/achievements/achievements.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Param, Get } from "@nestjs/common";
 import { AchievementService } from "./ahievements.service";
+import { UserAchievement } from "./entity/userAchievement";
 
 @Controller('achievement')
 export class AchievementController {
     constructor( private achievementsService: AchievementService) {}
+
+    @Post(':UserID/add/:achievement')
+    async addAchievement(@Param('UserID') userID: number, @Param('achievement') achievement: string) {
+        try{
+            await this.achievementsService.earnAchievement(+userID, achievement);
+        }
+        catch (Error){
+            console.log("Achievement was unable to be granted");
+            throw(Error);
+        }
+    }
+
+    @Get(':UserID')
+    async getAchievements(@Param('UserID') userID: number): Promise<UserAchievement[]> {
+        try{
+            return await this.achievementsService.getUserAchievements(+userID);
+        }
+        catch (Error){
+            console.log("Achievement get request failed");
+            throw(Error);
+        }
+    }
+
 }
